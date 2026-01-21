454cef5b548e61b4c7d1ff681beedba85252600e
Aleksandr Petrov <dzhiriki@gmail.com>
2026-01-21 13:32:53 -0600

    Initial data filling

---
+++ src/data/faction/index.ts
@@ -0,0 +1,58 @@
+import arborec from './arborec.json'
+import argent_flight from './argent_flight.json'
+import barony_of_letnev from './barony_of_letnev.json'
+import clan_of_saar from './clan_of_saar.json'
+import council_keleres from './council_keleres.json'
+import crimson_rebellion from './crimson_rebellion.json'
+import embers_of_muaat from './embers_of_muaat.json'
+import emirates_of_hacan from './emirates_of_hacan.json'
+import empyrean from './empyrean.json'
+import federation_of_sol from './federation_of_sol.json'
+import ghosts_of_creuss from './ghosts_of_creuss.json'
+import l1z1x_mindnet from './l1z1x_mindnet.json'
+import last_bastion from './last_bastion.json'
+import mahact_gene_sorcerers from './mahact_gene_sorcerers.json'
+import mentak_coalition from './mentak_coalition.json'
+import naalu_collective from './naalu_collective.json'
+import naaz_rokha_alliance from './naaz_rokha_alliance.json'
+import nekro_virus from './nekro_virus.json'
+import nomad from './nomad.json'
+import sardakk_norr from './sardakk_norr.json'
+import titans_of_ul from './titans_of_ul.json'
+import universities_of_jol_nar from './universities_of_jol_nar.json'
+import vuilraith_cabal from './vuilraith_cabal.json'
+import winnu from './winnu.json'
+import xxcha_kingdom from './xxcha_kingdom.json'
+import yin_brotherhood from './yin_brotherhood.json'
+import yssaril_tribes from './yssaril_tribes.json'
+
+// TE: DWS, Firmament, Ral Nel
+export default {
+  ARBOREC: arborec,
+  ARGENT_FLIGHT: argent_flight,
+  BARONY_OF_LETNEV: barony_of_letnev,
+  CLAN_OF_SAAR: clan_of_saar,
+  COUNCIL_KELERES: council_keleres,
+  CRIMSON_REBELLION: crimson_rebellion,
+  EMBERS_OF_MUAAT: embers_of_muaat,
+  EMIRATES_OF_HACAN: emirates_of_hacan,
+  EMPYREAN: empyrean,
+  FEDERATION_OF_SOL: federation_of_sol,
+  GHOSTS_OF_CREUSS: ghosts_of_creuss,
+  L1Z1X_MINDNET: l1z1x_mindnet,
+  LAST_BASTION: last_bastion,
+  MAHACT_GENE_SORCERERS: mahact_gene_sorcerers,
+  MENTAK_COALITION: mentak_coalition,
+  NAALU_COLLECTIVE: naalu_collective,
+  NAAZ_ROKHA_ALLIANCE: naaz_rokha_alliance,
+  NEKRO_VIRUS: nekro_virus,
+  NOMAD: nomad,
+  SARDAKK_NORR: sardakk_norr,
+  TITANS_OF_UL: titans_of_ul,
+  UNIVERSITIES_OF_JOL_NAR: universities_of_jol_nar,
+  VUILRAITH_CABAL: vuilraith_cabal,
+  WINNU: winnu,
+  XXCHA_KINGDOM: xxcha_kingdom,
+  YIN_BROTHERHOOD: yin_brotherhood,
+  YSSARIL_TRIBES: yssaril_tribes,
+}
