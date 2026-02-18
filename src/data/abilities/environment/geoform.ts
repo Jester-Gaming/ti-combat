e43c8cb13847cf5107735dd8cdec71e0d35c6f5b
Aleksandr Petrov <me@aleksandr.dev>
2026-02-17 19:48:32 -0600

    Add Geoform ability

---
+++ src/data/abilities/environment/geoform.ts
@@ -0,0 +1,24 @@
+import type { Unit } from '@/types'
+
+import type { Ability } from '../../../combat/abilities/types'
+
+export const geoform: Ability = {
+  key: 'GEOFORM',
+  name: 'Geoform',
+  category: 'ENVIRONMENT',
+  side: 'defender',
+  params: {
+    isEnabled: false,
+    uses: Infinity,
+  },
+  headerUI: 'isEnabled',
+  invoke: [
+    {
+      timing: 'BEFORE_UNIT_ABILITY_ROLL',
+      context: ['SPACE_CANNON_OFFENSE', 'SPACE_CANNON_DEFENSE'],
+      call: (_ctx, _params, dice) => {
+        dice.own.addDiceGroup('GEOFORM', {} as Unit, [5, 3])
+      },
+    },
+  ],
+}
--- src/data/abilities/environment/index.ts
+++ src/data/abilities/environment/index.ts
@@ -1,6 +1,7 @@
 import { dynamo } from './dynamo'
 import { entropicScar } from './entropic-scar'
+import { geoform } from './geoform'
 import { nebula } from './nebula'
 import { quietus } from './quietus'
 
-export default [dynamo, entropicScar, nebula, quietus]
+export default [dynamo, entropicScar, geoform, nebula, quietus]
