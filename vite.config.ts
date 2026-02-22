034a43e5bdb478a509106ddd4126f8eaf688e51c
Aleksandr Petrov <me@aleksandr.dev>
2026-02-22 00:46:12 -0600

    Add branch name to the title in dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
+import { execSync } from 'child_process'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    {
+      name: 'html-branch-title',
+      apply: 'serve',
+      transformIndexHtml(html) {
+        const branch = execSync('git rev-parse --abbrev-ref HEAD')
+          .toString()
+          .trim()
+        return html.replace(
+          /<title>(.*?)<\/title>/,
+          `<title>[${branch}] $1</title>`,
+        )
+      },
+    },
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
